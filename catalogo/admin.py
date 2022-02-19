ad7d83ac2ecf7deb1b2a976d1429bdeb5849a5c6
kelver-web <you@example.com>
2022-02-19 14:04:53 -0300

    ajustes no projeto

--- catalogo/admin.py
+++ catalogo/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Categoria, Produto
 
 # Register your models here.
+
+
+@admin.register(Produto)
+class ProdutoAdmin(admin.ModelAdmin):
+    list_display = ('nome_produto', 'categoria', 'quantidade', 'descricao',
+                    'publicado', 'ativo')
+    list_filter = ('nome_produto', 'categoria','criado_em')
+    search_fields = ('nome_produto', 'descricao')
+    ordering = ('nome_produto',)
+
+admin.site.register(Categoria)
--- catalogo/models.py
+++ catalogo/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+
+class Categoria(models.Model):
+    categoria = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.categoria
+
+
+class Produto(models.Model):
+    nome_produto = models.CharField(max_length=200)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    quantidade = models.IntegerField()
+    descricao = models.TextField()
+    publicado = models.DateTimeField(default=timezone.now)
+    criado_em = models.DateTimeField(auto_now_add=True)
+    modificado_em = models.DateTimeField(auto_now=True)
+    ativo = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.nome_produto
